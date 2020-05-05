c85b62ab48043b7903bf9c1b618c280b0e970377
beatrixyu <beatrixyu@126.com>
2020-05-05 23:07:19 +0200

    Updates

---
+++ precache-manifest.96d06df5417753c63cd8df0a83f3c75c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972868fe9f17a4f6475313544a4fcb41",
+    "url": "/tetris-game-react/index.html"
+  },
+  {
+    "revision": "4984f44ce4090a67470d",
+    "url": "/tetris-game-react/static/js/2.de0aaf91.chunk.js"
+  },
+  {
+    "revision": "9b56c8365084d558d6f2",
+    "url": "/tetris-game-react/static/js/main.45550493.chunk.js"
+  },
+  {
+    "revision": "a22a727b756295ebb30b",
+    "url": "/tetris-game-react/static/js/runtime~main.5ab86a1e.js"
+  },
+  {
+    "revision": "3bfdb34325ffd7e7385a29046ae0cd73",
+    "url": "/tetris-game-react/static/media/pocoyo.3bfdb343.jpg"
+  }
+]);
